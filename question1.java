40e35a584add5572d15568d4eeeff1524b073780
vrushang desai <bulldesai@macbookpro.local>
2018-06-14 20:36:04 +0530

    Cube

---
+++ question1.java
@@ -0,0 +1,11 @@
+import static java.lang.Boolean.compare;
+import static java.lang.Byte.compare;
+import static java.lang.Character.compare;
+import java.util.Scanner;
+
+ class question1 {
+
+    public static void main(String args[]){
+        System.out.println("Hello World !");
+    }
+}
