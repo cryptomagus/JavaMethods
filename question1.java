201aade6d24dca14d263ae05727b4f1d30a88eb2
vrushang desai <bulldesai@macbookpro.local>
2018-06-14 20:50:13 +0530

    cube program done iusing methods in java

--- question1.java
+++ question1.java
@@ -2,10 +2,24 @@ import static java.lang.Boolean.compare;
 import static java.lang.Byte.compare;
 import static java.lang.Character.compare;
 import java.util.Scanner;
-
+//
  class question1 {
 
     public static void main(String args[]){
-        System.out.println("Hello World !");
+    		System.out.println("Enter Number You Want To Cube ?");
+    		Scanner scan =new Scanner(System.in);
+    		int x = scan.nextInt();
+    		cube(x);
+
     }
+
+
+
+    public static void cube(int x){
+	int cube = x*x*x;
+
+	System.out.println("Cube Of The Number " + x + " is " + cube);
+	}
 }
+
+
